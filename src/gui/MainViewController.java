e9c0d0e1fb7a14013061a3aec042d4ff97402fcb
Gabriel Sousa <gsousax2008@hotmail.com>
2021-02-23 13:59:29 -0300

    adicionado controladores para a barra de ferramentas

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,39 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuitemVendedor;
+    @FXML
+    private MenuItem menuitemDepartamento;
+    @FXML
+    private MenuItem MenuItemSobre;
+
+    @FXML
+    public void onMenuItemVendedorAction() {
+
+    }
+
+    @FXML
+    public void onMenuSobreAction() {
+
+    }
+
+    @FXML
+
+    public void onMenuDepartamentoAction() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
